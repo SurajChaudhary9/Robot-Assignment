7132ec15c7c684ec5e0965668a8d29a2c2bd35a8
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2023-01-04 17:54:12 +0530

    [Suraj] Robot Positioning Assignment

---
+++ src/test/java/com/nunify/RobotTest.java
@@ -0,0 +1,23 @@
+package com.nunify;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RobotTest {
+
+    @Test
+    public void
+    testInputOneNorth() {
+        String newPosition = Robot.move(1, 0, 'N', "MMRMMLMMR");
+        assertEquals("3 4 E", newPosition);
+    }
+
+    @Test
+    public void
+    testInputTwoEast() {
+        String newPosition = Robot.move(3, 2, 'E', "MLLMMRMM");
+        assertEquals("2 4 N", newPosition);
+    }
+
+}
